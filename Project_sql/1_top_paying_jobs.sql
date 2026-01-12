e73b88489a9f26a4b18730ae6886a19651fe442c
Karim <Km678166@gmail.com>
2026-01-12 23:15:56 +0200

    upload sql files

---
+++ Project_sql/1_top_paying_jobs.sql
@@ -0,0 +1,21 @@
+/*   What are the top-paying data analyst jobs?
+    Identify the top 10 highest-paying Data Analyst roles that are available remotely.
+    Focuses on job postings with specified salaries and includes the company name for each role.
+    Why? Aims to highlight the top-paying opportunities for Data Analysts, offering insights into top employers, employment options, and location flexibility.
+*/
+
+SELECT job_id,
+    job_title,
+	job_location,
+	job_schedule_type,
+	salary_year_avg,
+	job_posted_date,
+    name AS company_name
+FROM 
+    job_postings_fact
+LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
+WHERE 
+    job_title = 'Data Analyst' AND job_location = 'Anywhere' AND salary_year_avg IS NOT NULL
+ORDER BY 
+    salary_year_avg DESC
+LIMIT 10
